95f7d5a7ddee547674df0922921efe978d1a88d2
Esubalew Chekol <esubalew.chekol-ug@aau.edu.et>
2024-03-24 11:34:49 +0300

    add get_word_of_the_day functon

--- dictionary/britannica.py
+++ dictionary/britannica.py
@@ -65,4 +65,46 @@ def get_total_entries(word):
     return len(get_entries(word))
 
 
-print(get_entries('apple'))
+def get_word_of_the_day():
+    '''
+    Get the word of the day from Britannica Dictionary
+
+    Returns:
+        dict: Dictionary containing the word, part of speech, image, and meaning information, or None if not found
+    '''
+    url = f"{DOMAIN}/dictionary/eb/word-of-the-day"
+    soup = get_soup(url)
+    
+    if not soup:
+        return None
+
+    word_container = soup.find('div', class_='hw_d box_sizing ld_xs_hidden')
+    image_container = soup.find('div', class_='wod_img_act')
+    meaning_container = soup.find('div', class_='midbs')  # Container for meaning information
+
+    word_info = {}
+
+    if word_container:
+        word_text = word_container.find('span', class_='hw_txt').get_text(strip=True)
+        part_of_speech = word_container.find('span', class_='fl').get_text(strip=True)
+        word_info['word'] = f"{word_text} ({part_of_speech})"
+    else:
+        return None
+
+    if image_container:
+        image = image_container.find('img')
+        if image:
+            word_info['image'] = {'src': image.get('src', ''), 'alt': image.get('alt', '')}
+
+    if meaning_container:
+        meanings = []
+        meaning_blocks = meaning_container.find_all('div', class_='midb')
+        for block in meaning_blocks:
+            definition = block.find('div', class_='midbt').find('p').get_text(strip=False)
+            examples = [example.get_text(strip=False) for example in block.find_all('li', class_='vi')]
+            meaning = { 'definition': definition, 'examples': examples}
+            meanings.append(meaning)
+    word_info['meanings'] = meanings
+
+
+    return word_info
