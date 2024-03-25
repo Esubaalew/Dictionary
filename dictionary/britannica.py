058b7cbc04d49ae11f20c5b508cb6d372379a0e6
Esubalew Chekol <esubalew.chekol-ug@aau.edu.et>
2024-03-25 09:37:12 +0300

    add tools for common functions

--- dictionary/britannica.py
+++ dictionary/britannica.py
@@ -4,33 +4,11 @@ Module for Britannica Dictionary
 
 '''
 
-import requests
-from bs4 import BeautifulSoup
+from dictionary.tools import get_soup
 
 DOMAIN = 'https://www.britannica.com'
 
-def get_soup(url):
-    '''
-    Get BeautifulSoup object for a URL
 
-    Args:
-        url (str): URL to fetch and parse
-
-    Returns:
-        BeautifulSoup: BeautifulSoup object for the URL, or None if unable to retrieve
-    '''
-    try:
-        response = requests.get(url)
-        response.raise_for_status()  # Raise an exception for bad status codes
-        soup = BeautifulSoup(response.text, 'html.parser')
-        return soup
-    except requests.RequestException as e:
-        print(f"Error fetching URL: {e}")
-        return None
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        return None
-    
 def get_entries(word):
     '''
     Get related entries for a word in Britannica Dictionary
