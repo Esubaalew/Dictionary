6ca82b0832040ab3c95f3f0446565be71ebdbd86
esubaalew <esubalewchekol6@gmail.com>
2025-10-15 09:54:13 +0300

    Add GitHub workflow for PyPI publishing and bump version to 0.1.2

---
+++ dictionary/__init__.py
@@ -0,0 +1,18 @@
+__version__ = "0.1.2"
+
+from dictionary.britannica import (
+    get_entries,
+    get_total_entries,
+    get_word_of_the_day,
+    get_parts,
+    get_definitions
+)
+
+__all__ = [
+    'get_entries',
+    'get_total_entries',
+    'get_word_of_the_day',
+    'get_parts',
+    'get_definitions'
+]
+
